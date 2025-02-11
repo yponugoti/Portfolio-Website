675efa6247c44aa26652de8900d8f81eee5a5efc
yaswanth <yaswanth.p31@gmail.com>
2025-02-10 23:42:41 -0700

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import styles from './Hero.module.css';
+import HeroPfp from './assets/HeroPfp.jpeg'
+
+
+function App() {
+  return (
+    <Component id="hero">
+      <div>
+        <img src={HeroPfp} alt="" />
+      </div>
+    </Component>
+  );
+}
+
+export default App
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+import styles from './Hero.module.css';
+
+function Hero() {
+  return <Component id="hero">
+    <div>
+        <img src="" alt="" />
+    </div>
+  </Component>;
+}
+
+export default Hero;
